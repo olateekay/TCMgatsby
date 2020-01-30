cfe96d95e48241fb8fe5a1cad7b08099f98f3dc5
Olatokunbo <olatokunboalege@gmail.com>
2020-01-30 04:01:32 +0100

    Passed TCM logo as props into the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -16,6 +16,7 @@ export const Header = styled(({ siteTitle, ...styleProps }) => {
             <SiteTitle>
               <SiteLink to="/">
                 {/* <Coffee /> */}
+                <img src={require('../../content/images/icon.png')} alt=''/>
                 {siteTitle}
               </SiteLink>
             </SiteTitle>
